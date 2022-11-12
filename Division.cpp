293c2baaae449fc263ee33abf83f383616dba5b9
Ashish Chachane <117847834+aashishchachane@users.noreply.github.com>
2022-11-12 16:32:15 +0530

    Add files via upload

---
+++ Division.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,b,c;
+	
+	cout<<"Enter the value of a : ";
+	cin>>a;
+	cout<<"Enter the value of b : ";
+	cin>>b;
+	
+	c=a/b;
+	
+	cout<<"The Result of division is : "<<c;
+	
+	return 0;	
+}
