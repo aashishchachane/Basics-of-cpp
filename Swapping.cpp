2f5922664fe23b015695d69f25076f5704731d32
Ashish Chachane <117847834+aashishchachane@users.noreply.github.com>
2022-11-12 16:24:10 +0530

    Add files via upload

---
+++ Swapping.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cout<<"Enter the value of a : ";
+	cin>>a;
+	cout<<"Enter the value of b : ";
+	cin>>b;
+	cout<<endl;
+	
+	c=a;
+	a=b;
+	b=c; 
+		
+	cout <<"Boom !!! Swapping of given values has been done."<<endl<<endl;
+	
+	cout<<"The value of a is : "<<a<<endl;
+	cout<<"The value of b is : "<<b;
+	
+	return 0;
+	
+}
